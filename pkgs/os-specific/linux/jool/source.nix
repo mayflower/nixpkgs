12e30bebf6f4852b3735bd9c6927fc319e0c4dcc
Franz Pletz <fpletz@fnordicwalking.de>
2015-10-19 04:00:32 +0200

    jool: 3.3.2 -> 3.3.5

--- pkgs/os-specific/linux/jool/source.nix
+++ pkgs/os-specific/linux/jool/source.nix
@@ -1,9 +1,9 @@
 { fetchzip }:
 
 rec {
-  version = "3.3.2";
+  version = "3.3.5";
   src = fetchzip {
     url = "https://www.jool.mx/download/Jool-${version}.zip";
-    sha256 = "0hc6vlxzmjrgf7vjcwprdqcbx3biq8kphks5k725mrd9rb84drgw";
+    sha256 = "00ly17ws0dyykvljlvy0c2gyc59l5r39amhgb8plfwhcxvizapi1";
   };
 }
