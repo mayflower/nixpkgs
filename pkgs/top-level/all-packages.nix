fe8e31b446d80aa490b02f04f3882514e5912c7d
Franz Pletz <fpletz@fnordicwalking.de>
2015-11-07 06:30:42 +0100

    pycurl: Needs curl linked against openssl

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1208,6 +1208,10 @@ let
     gssSupport = true;
   };
 
+  curlOpenssl = curl.override {
+    openssl = openssl_1_0_1;
+  };
+
   curl = callPackage ../tools/networking/curl rec {
     fetchurl = fetchurlBoot;
     zlibSupport = true;
