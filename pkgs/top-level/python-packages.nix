7b19f125fb52d2724d8259ac3c8301bcc9035f1c
Franz Pletz <fpletz@fnordicwalking.de>
2015-11-16 02:38:56 +0100

    urllib3: Add patch to fix sslv3 test with libressl

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4012,6 +4012,8 @@ in modules // {
 
     buildInputs = with self; [ coverage tornado mock nose ];
 
+    patches = [ ../development/python-modules/urllib3-fix-sslv3-test.patch ];
+
     meta = {
       description = "A Python library for Dropbox's HTTP-based Core and Datastore APIs";
       homepage = https://www.dropbox.com/developers/core/docs;
