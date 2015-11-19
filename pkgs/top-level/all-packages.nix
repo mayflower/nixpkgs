511ca019381b85621e3253a464c3f5ee3f3ea549
Franz Pletz <fpletz@fnordicwalking.de>
2015-11-19 15:00:08 +0100

    charybdis-darkfasel: init fork

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9161,6 +9161,7 @@ let
   scollector = bosun;
 
   charybdis = callPackage ../servers/irc/charybdis {};
+  charybdis-darkfasel = callPackage ../servers/irc/charybdis/darkfasel.nix {};
 
   couchdb = callPackage ../servers/http/couchdb {
     spidermonkey = spidermonkey_185;
