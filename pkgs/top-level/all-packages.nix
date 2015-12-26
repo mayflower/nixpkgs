b6c1aa0c7c310d1717a6716c3749628b157fc329
Robin Gloster <mail@glob.in>
2015-12-26 03:30:10 +0000

    ruby: remove unused versions

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5356,8 +5356,8 @@ let
   inherit (callPackage ../development/interpreters/ruby {})
     ruby_1_9_3
     ruby_2_0_0
-    ruby_2_1_0 ruby_2_1_1 ruby_2_1_2 ruby_2_1_3 ruby_2_1_6 ruby_2_1_7
-    ruby_2_2_0 ruby_2_2_2 ruby_2_2_3;
+    ruby_2_1_3 ruby_2_1_7
+    ruby_2_2_2 ruby_2_2_3;
 
   # Ruby aliases
   ruby = ruby_2_2;
