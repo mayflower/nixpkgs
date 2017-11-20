da11627cf95bbbf2d03c60bc17a304cbac45942e
Robin Gloster <mail@glob.in>
2017-11-20 02:27:50 +0100

    mailman: hyperkitty patching part 1

--- pkgs/servers/mail/mailman/default.nix
+++ pkgs/servers/mail/mailman/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitLab, python3, hyperkitty }:
+{ stdenv, fetchFromGitLab, python3 }:
 
 python3.pkgs.buildPythonApplication rec {
   name = "mailman-${version}";
@@ -16,12 +16,12 @@ python3.pkgs.buildPythonApplication rec {
       --replace "config.BIN_DIR, 'master'" "config.BIN_DIR, '.master-wrapped'"
     substituteInPlace src/mailman/bin/master.py \
       --replace "config.BIN_DIR, 'runner'" "config.BIN_DIR, '.runner-wrapped'"
+    sed -i '/PRESERVE_ENVS =/ a"PYTHONHOME",' src/mailman/bin/master.py
   '';
 
   propagatedBuildInputs = with python3.pkgs; [
     aiosmtpd alembic atpublic falcon flufl-bounce flufl-i18n flufl-lock flufl-testing dns
     httplib2 lazr-config lazr-smtptest nose nose2 passlib psycopg2 requests zope_component
-    hyperkitty
   ];
 
   # checkPhase = ''
