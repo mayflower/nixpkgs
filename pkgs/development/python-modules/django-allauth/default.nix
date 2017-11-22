bd64c6a05d175c32eb03662aed08f95f7f40916f
Robin Gloster <mail@glob.in>
2017-11-22 18:04:09 +0100

    python.pkgs.django-allauth: 0.33.0 -> 0.34.0

--- pkgs/development/python-modules/django-allauth/default.nix
+++ pkgs/development/python-modules/django-allauth/default.nix
@@ -1,20 +1,29 @@
-{ stdenv, buildPythonPackage, fetchPypi, requests, requests_oauthlib
-, django, python3-openid }:
+{ stdenv, buildPythonPackage, fetchFromGitHub, requests, requests_oauthlib
+, django, python-openid, mock, coverage }:
 
 buildPythonPackage rec {
   pname = "django-allauth";
   name = "${pname}-${version}";
-  version = "0.33.0";
+  version = "0.34.0";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "0g8akxpk1gywzy40xkpxxdn67zbmir7l4lpzd96ksz3h945shr5f";
+  # no tests on PyPI
+  src = fetchFromGitHub {
+    owner = "pennersr";
+    repo = pname;
+    rev = version;
+    sha256 = "1xbvgng2hgkygmvlymlc8k80lj4mn6d93in1jcc4619agk6jr90d";
   };
 
-  propagatedBuildInputs = [ requests requests_oauthlib django python3-openid ];
+  propagatedBuildInputs = [ requests requests_oauthlib django python-openid ];
+
+  checkInputs = [ coverage mock ];
 
   doCheck = false;
-  
+  checkPhase = ''
+    cd $NIX_BUILD_TOP/$sourceRoot
+    coverage run manage.py test allauth
+  '';
+
   meta = with stdenv.lib; {
     description = "Integrated set of Django applications addressing authentication, registration, account management as well as 3rd party (social) account authentication";
     homepage = https://www.intenct.nl/projects/django-allauth;
