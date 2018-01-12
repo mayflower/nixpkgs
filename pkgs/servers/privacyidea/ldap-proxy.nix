71cc687419fa0412fb2e0cc1d1707178c38e4788
Franz Pletz <fpletz@fnordicwalking.de>
2018-01-12 01:50:20 +0100

    privacyidea-ldap-proxy: 0.3 -> 0.5

--- pkgs/servers/privacyidea/ldap-proxy.nix
+++ pkgs/servers/privacyidea/ldap-proxy.nix
@@ -1,26 +1,38 @@
 { stdenv, pythonPackages, fetchFromGitHub }:
 
-pythonPackages.buildPythonApplication rec {
-  name = "privacyidea-ldap-proxy-${version}";
-  version = "0.3";
+let
 
-  src = fetchFromGitHub {
-    owner = "NetKnights-GmbH";
-    repo = "privacyidea-ldap-proxy";
-    rev = "v${version}";
-    sha256 = "0g2idpkp3wagkrn7v6wngsi6jkhp5c13npypk56gv6fndwdg6140";
+  self = pythonPackages.buildPythonApplication rec {
+    name = "privacyidea-ldap-proxy-${version}";
+    version = "0.5";
+
+    src = fetchFromGitHub {
+      owner = "NetKnights-GmbH";
+      repo = "privacyidea-ldap-proxy";
+      rev = "v${version}";
+      sha256 = "0zmdrym7hb2x7bhv1xsrai0lyznc51kv4gxlzs0na9lv41a632bi";
+    };
+
+    checkPhase = ''
+      trial pi_ldapproxy.test
+    '';
+
+    propagatedBuildInputs = with pythonPackages; [ twisted ldaptor configobj ]
+      ++ twisted.extras.tls;
+
+    passthru.env = pythonPackages.python.buildEnv.override {
+      extraLibs = propagatedBuildInputs ++ [self];
+    };
+
+    passthru.python = pythonPackages.python;
+
+    meta = with stdenv.lib; {
+      description = "PrivacyIDEA LDAP Proxy";
+      license = licenses.agpl3Plus;
+      platforms = platforms.unix;
+      maintainers = with maintainers; [ fpletz globin ];
+      priority = 10;
+    };
   };
 
-  checkPhase = ''
-    trial pi_ldapproxy.test
-  '';
-
-  propagatedBuildInputs = with pythonPackages; [ twisted ldaptor configobj ];
-
-  meta = with stdenv.lib; {
-    description = "PrivacyIDEA LDAP Proxy";
-    license = licenses.agpl3Plus;
-    platforms = platforms.unix;
-    maintainers = with maintainers; [ fpletz globin ];
-  };
-}
+in self
