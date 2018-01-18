7012b8fa31343eb699da7c6894234aff94c290ef
Franz Pletz <fpletz@fnordicwalking.de>
2018-01-19 00:10:27 +0100

    rspamd module: add proxy milter support

--- nixos/modules/services/mail/rspamd.nix
+++ nixos/modules/services/mail/rspamd.nix
@@ -31,6 +31,17 @@ let
       ${mkBindSockets cfg.bindUISocket}
         .include "$CONFDIR/worker-controller.inc"
       }
+
+      worker "rspamd_proxy" {
+      ${mkBindSockets cfg.bindMilter}
+        .include "$CONFDIR/worker-proxy.inc"
+        .include(try=true; priority=10) "${pkgs.writeText "rspamd-worker-proxy.inc" ''
+          upstream "local" {
+            default = true;
+            self_scan = yes;
+          }
+        ''}"
+      }
    '';
 
 in
@@ -52,20 +63,34 @@ in
 
       bindSocket = mkOption {
         type = types.listOf types.str;
-        default = [
-          "/run/rspamd/rspamd.sock mode=0660 owner=${cfg.user} group=${cfg.group}"
+        default = [ "/run/rspamd/rspamd.sock mode=0660 owner=rspamd group=rspamd" ];
+        example = [
+          "/run/rspamd.sock mode=0666 owner=rspamd"
+          "*:11333"
         ];
-        defaultText = ''[
-          "/run/rspamd/rspamd.sock mode=0660 owner=${cfg.user} group=${cfg.group}"
-        ]'';
         description = ''
           List of sockets to listen, in format acceptable by rspamd
         '';
-        example = ''
-          bindSocket = [
-            "/run/rspamd.sock mode=0666 owner=rspamd"
-            "*:11333"
-          ];
+      };
+
+      bindMilter = mkOption {
+        type = types.listOf types.str;
+        default = [ "/run/rspamd/milter.sock mode=0660 owner=rspamd group=rspamd" ];
+        example = [
+          "/run/rspamd.sock mode=0666 owner=rspamd"
+          "*:11332"
+        ];
+        description = ''
+          List of proxy milter sockets to listen, in format acceptable by rspamd
+        '';
+      };
+
+      enablePostfix = mkOption {
+        type = types.bool;
+        default = false;
+        description = ''
+          Integrate rspamd proxy milter via <literal>/run/rspamd/milter.sock</literal>
+          into the local Postfix config.
         '';
       };
 
@@ -85,7 +110,7 @@ in
         description = ''
           User to use when no root privileges are required.
         '';
-       };
+      };
 
       group = mkOption {
         type = types.string;
@@ -100,40 +125,56 @@ in
 
   ###### implementation
 
-  config = mkIf cfg.enable {
+  config = mkMerge [
+    (mkIf cfg.enable {
+      # Allow users to run 'rspamc' and 'rspamadm'.
+      environment.systemPackages = [ pkgs.rspamd ];
 
-    # Allow users to run 'rspamc' and 'rspamadm'.
-    environment.systemPackages = [ pkgs.rspamd ];
-
-    users.extraUsers = singleton {
-      name = cfg.user;
-      description = "rspamd daemon";
-      uid = config.ids.uids.rspamd;
-      group = cfg.group;
-    };
-
-    users.extraGroups = singleton {
-      name = cfg.group;
-      gid = config.ids.gids.rspamd;
-    };
-
-    systemd.services.rspamd = {
-      description = "Rspamd Service";
-
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network.target" ];
-
-      serviceConfig = {
-        ExecStart = "${pkgs.rspamd}/bin/rspamd ${optionalString cfg.debug "-d"} --user=${cfg.user} --group=${cfg.group} --pid=/run/rspamd.pid -c ${rspamdConfFile} -f";
-        Restart = "always";
-        RuntimeDirectory = "rspamd";
-        PrivateTmp = true;
+      services.rspamd = {
+        bindSocket = mkDefault [ "/run/rspamd/rspamd.sock mode=0660 owner=${cfg.user} group=${cfg.group}" ];
+        bindMilter = mkDefault [ "/run/rspamd/milter.sock mode=0660 owner=${cfg.user} group=${cfg.group}" ];
       };
 
-      preStart = ''
-        ${pkgs.coreutils}/bin/mkdir -p /var/lib/rspamd
-        ${pkgs.coreutils}/bin/chown ${cfg.user}:${cfg.group} /var/lib/rspamd
-      '';
-    };
-  };
+      users.extraUsers = singleton {
+        name = cfg.user;
+        description = "rspamd daemon";
+        uid = config.ids.uids.rspamd;
+        group = cfg.group;
+      };
+
+      users.extraGroups = singleton {
+        name = cfg.group;
+        gid = config.ids.gids.rspamd;
+      };
+
+      systemd.services.rspamd = {
+        description = "Rspamd Service";
+
+        wantedBy = [ "multi-user.target" ];
+        after = [ "network.target" ];
+
+        serviceConfig = {
+          ExecStart = "${pkgs.rspamd}/bin/rspamd ${optionalString cfg.debug "-d"} --user=${cfg.user} --group=${cfg.group} --pid=/run/rspamd.pid -c ${rspamdConfFile} -f";
+          Restart = "always";
+          RuntimeDirectory = "rspamd";
+          PrivateTmp = true;
+        };
+
+        preStart = ''
+          ${pkgs.coreutils}/bin/mkdir -p /var/lib/rspamd
+          ${pkgs.coreutils}/bin/chown ${cfg.user}:${cfg.group} /var/lib/rspamd
+        '';
+      };
+    })
+
+    (mkIf (cfg.enable && cfg.enablePostfix) {
+      services.postfix.config = {
+        smtpd_milters = [ "unix:/run/rspamd/milter.sock" ];
+        milter_protocol = "6";
+        milter_mail_macros = "i {mail_addr} {client_addr} {client_name} {auth_authen}";
+        milter_default_action = mkDefault "tempfail";
+      };
+      users.extraUsers.${config.services.postfix.user}.extraGroups = [ cfg.group ];
+    })
+  ];
 }
