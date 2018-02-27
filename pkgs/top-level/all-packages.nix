dbd4742af2792100c5b083961805c5d21739b8a3
Bignaux Ronan <ronan@aimao.org>
2018-02-27 19:02:44 +0100

    xcpc: init at 20070122

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20875,6 +20875,8 @@ with pkgs;
 
   zsnes = callPackage_i686 ../misc/emulators/zsnes { };
 
+  xcpc = callPackage ../misc/emulators/xcpc { };
+
   zxcvbn-c = callPackage ../development/libraries/zxcvbn-c { };
 
   snes9x-gtk = callPackage ../misc/emulators/snes9x-gtk { };
