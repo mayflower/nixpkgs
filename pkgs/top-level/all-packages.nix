e3a62cbcd4a0d9f0d2594c2706e5aabd46744b26
Charles Strahan <charles@cstrahan.com>
2018-03-17 01:35:35 -0400

    nixos: kubernetes fixes
    * Fix reference CNI plugins
      * The plugins were split out of the upstream cni repo around version
        0.6.0
    
    * Fix RBAC and DNS tests
      * Fix broken apiVersion fields
      * Change plugin linking to look in ${package}/bin rather than
        ${package.plugins}
    
    * Initial work towards a working e2e test
      * Test still fails, but at least the expression evaluates now
    
    Continues @srhb's work in #37199
    
    Fixes #37199
    
    (cherry picked from commit 709b6f664e472ae2075bbee83bfb1dd1ba95e0e7)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15170,6 +15170,7 @@ with pkgs;
   };
 
   cni = callPackage ../applications/networking/cluster/cni {};
+  cni-plugins = callPackage ../applications/networking/cluster/cni/plugins.nix {};
 
   communi = libsForQt5.callPackage ../applications/networking/irc/communi { };
 
