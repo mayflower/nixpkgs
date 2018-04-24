86d6e498ba4806b0d73e859948b1c0b9139ac092
Franz Pletz <fpletz@fnordicwalking.de>
2018-04-24 18:55:57 +0200

    fix bad merge (first 18.03 merge)

--- pkgs/desktops/xfce/applications/parole.nix
+++ pkgs/desktops/xfce/applications/parole.nix
@@ -15,19 +15,20 @@ stdenv.mkDerivation rec {
   };
   name = "${p_name}-${ver_maj}.${ver_min}";
 
-  nativeBuildInputs = [ pkgconfig intltool wrapGAppsHook ];
+  nativeBuildInputs = [ pkgconfig intltool ];
 
   buildInputs = [
     makeWrapper hicolor-icon-theme
     gtk dbus-glib libxfce4ui libxfce4util xfconf
     taglib libnotify
-  ] ++ (stdenv.lib.optionals withGstPlugins
-    (with gst_all_1; [
-      gst-plugins-base gst-plugins-good gst-plugins-bad
-      gst-plugins-ugly gst-libav
-    ]));
+  ] ++ (with gst_all_1; [ gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav]);
 
   configureFlags = [ "--with-gstreamer=1.0" ];
+
+  postInstall = stdenv.lib.optionalString withGstPlugins ''
+    wrapProgram "$out/bin/parole" --prefix \
+      GST_PLUGIN_SYSTEM_PATH_1_0 ":" "$GST_PLUGIN_SYSTEM_PATH_1_0"
+  '';
 
   meta = {
     homepage = "http://goodies.xfce.org/projects/applications/${p_name}";
