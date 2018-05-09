9156a1ee144da62526c1a5937243644d8e936a40
Robin Gloster <mail@glob.in>
2018-05-10 01:01:21 +0200

    acpid: fix cross

--- pkgs/os-specific/linux/acpid/default.nix
+++ pkgs/os-specific/linux/acpid/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchurl, autoreconfHook }:
 
 stdenv.mkDerivation rec {
   name = "acpid-2.0.28";
@@ -7,6 +7,15 @@ stdenv.mkDerivation rec {
     url = "mirror://sourceforge/acpid2/${name}.tar.xz";
     sha256 = "043igasvp1l6nv5rzh4sksmymay2qn20anl4zm4zvwnkn1a3l34q";
   };
+
+  nativeBuildInputs = [ autoreconfHook ];
+
+  postPatch = ''
+    substituteInPlace configure.ac \
+      --replace "AC_FUNC_MALLOC" "" \
+      --replace "AC_FUNC_REALLOC" "" \
+      --replace "strrchr strtol" "strrchr strtol malloc realloc"
+  '';
 
   meta = with stdenv.lib; {
     homepage = http://tedfelix.com/linux/acpid-netlink.html;
