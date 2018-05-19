43f198068db5000ce3a70cde4c8292db620410ba
Kirill Elagin <kirelagin@gmail.com>
2018-05-19 13:08:04 +0300

    wireguard: use fetchzip instead of fetchurl
    Because cgit snapshots are not deterministic.
    
    (cherry picked from commit 95cf07fc95d869c433ad96aaaca5a09f1065c97e)
    (cherry picked from commit be85a7f8cc2003c0a058d3de494ad64f37b0f7dd)

--- pkgs/os-specific/linux/wireguard/default.nix
+++ pkgs/os-specific/linux/wireguard/default.nix
@@ -1,15 +1,15 @@
-{ stdenv, fetchurl, kernel }:
+{ stdenv, fetchzip, kernel }:
 
 # module requires Linux >= 3.10 https://www.wireguard.io/install/#kernel-requirements
 assert stdenv.lib.versionAtLeast kernel.version "3.10";
 
 stdenv.mkDerivation rec {
   name = "wireguard-${version}";
-  version = "0.0.20180519";
+  version = "0.0.20180514";
 
-  src = fetchurl {
+  src = fetchzip {
     url    = "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-${version}.tar.xz";
-    sha256 = "0qn4w4h19g3wyi67p4kcymcy5099rif9hk4anfdhfzizdh0b6il8";
+    sha256 = "15z0s1i8qyq1fpw8j6rky53ffrpp3f49zn1022jwdslk4g0ncaaj";
   };
 
   preConfigure = ''
