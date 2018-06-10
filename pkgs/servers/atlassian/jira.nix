d558e1ff723e1d13534bdb0c24739f1670feddb9
Simon Lackerbauer <simon@lackerbauer.com>
2018-06-10 11:01:53 +0200

    atlassian-jira: 7.9.2 -> 7.10.0

--- pkgs/servers/atlassian/jira.nix
+++ pkgs/servers/atlassian/jira.nix
@@ -5,11 +5,11 @@
 
 stdenv.mkDerivation rec {
   name = "atlassian-jira-${version}";
-  version = "7.9.2";
+  version = "7.10.0";
 
   src = fetchurl {
     url = "https://downloads.atlassian.com/software/jira/downloads/atlassian-jira-software-${version}.tar.gz";
-    sha256 = "05976h6033v2w7d05qnigxmsrm33bg7gmgyzpvis8910fkxrhvhh";
+    sha256 = "1805v8ag0aflyygc194xk696xk5krbc8zc4yba4cx95dcnc4rvwq";
   };
 
   buildPhase = ''
