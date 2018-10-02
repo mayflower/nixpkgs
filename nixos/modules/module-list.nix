6cfcc7f47bb739e421a38757ba89903ecf5095e3
Victor SENE <victor@sene.ovh>
2018-10-02 11:05:20 +0200

    nixos/roundcube: add to module-list
    (cherry picked from commit 4497af9a826d8d694d520e5e105184efecacfeca)

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -321,6 +321,8 @@
   ./services/mail/postgrey.nix
   ./services/mail/spamassassin.nix
   ./services/mail/rspamd.nix
+  ./services/mail/rmilter.nix
+  ./services/mail/roundcube.nix
   ./services/mail/nullmailer.nix
   ./services/misc/airsonic.nix
   ./services/misc/apache-kafka.nix
