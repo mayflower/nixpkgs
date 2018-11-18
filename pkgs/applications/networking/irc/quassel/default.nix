3860db34f23aca7475e18d90a4381e9e3d108bb1
Franz Pletz <fpletz@fnordicwalking.de>
2018-11-18 08:42:34 +0100

    quassel: fix typo (merge fnord)

--- pkgs/applications/networking/irc/quassel/default.nix
+++ pkgs/applications/networking/irc/quassel/default.nix
@@ -71,7 +71,7 @@ in with stdenv; mkDerivation rec {
           --prefix GIO_EXTRA_MODULES : "${dconf}/lib/gio/modules"
     '';
 
-  patches = sources.patches ++ [ ./qt5_11.patch ];
+  patches = source.patches ++ [ ./qt5_11.patch ];
 
   meta = with stdenv.lib; {
     homepage = https://quassel-irc.org/;
