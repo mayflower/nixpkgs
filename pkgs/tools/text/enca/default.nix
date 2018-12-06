b0acddaffe27055a9ee7892d5c86a6b2f42b057b
Robin Gloster <mail@glob.in>
2018-12-06 17:17:02 +0100

    enca: fix cross

--- pkgs/tools/text/enca/default.nix
+++ pkgs/tools/text/enca/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, libiconv, recode }:
+{ stdenv, fetchurl, fetchpatch, buildPackages, autoreconfHook, libiconv, recode }:
 
 stdenv.mkDerivation rec {
   name = "enca-${version}";
@@ -9,7 +9,16 @@ stdenv.mkDerivation rec {
     sha256 = "1f78jmrggv3jymql8imm5m9yc8nqjw5l99mpwki2245l8357wj1s";
   };
 
+  depsBuildBuild = [ buildPackages.stdenv.cc ];
+  nativeBuildInputs = [ autoreconfHook ];
   buildInputs = [ recode libiconv ];
+
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/nijel/enca/commit/2393833d133a6784e57215b89e4c4c0484555985.patch";
+      sha256 = "05m70lfmkxlhpigxg0yhs1v2wlb21bw62dgxsca2pnm0s0qznplb";
+    })
+  ];
 
   meta = with stdenv.lib; {
     description = "Detects the encoding of text files and reencodes them";
@@ -23,6 +32,5 @@ stdenv.mkDerivation rec {
     '';
 
     license = licenses.gpl2;
-   
   };
 }
