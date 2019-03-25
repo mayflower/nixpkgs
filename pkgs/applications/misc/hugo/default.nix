fd8f3299d708cabd522e9b88559b607dee201443
Robin Gloster <mail@glob.in>
2019-03-25 15:41:18 +0100

    hugo: rm obsolete patch

--- pkgs/applications/misc/hugo/default.nix
+++ pkgs/applications/misc/hugo/default.nix
@@ -13,13 +13,6 @@ buildGoPackage rec {
     sha256 = "01grfbr3kpd4qf5cbcmzc6yfq34cm0nkak4pqzgrn46r254y0ymv";
   };
 
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/gohugoio/hugo/commit/b137ad4dbd6d14d0a9af68c044aaee61f2c87fe5.diff";
-      sha256 = "0w1gpg11idqywqcpwzvx4xabn02kk8y4jmyz4h67mc3yh2dhq3ll";
-    })
-  ];
-
   goDeps = ./deps.nix;
 
   buildFlags = "-tags extended";
