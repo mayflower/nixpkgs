6977e41bd7fff030d85ae86655238aa45e1504d9
Simon Lackerbauer <simon@lackerbauer.com>
2019-04-08 17:17:00 +0200

    atlassian-jira: 8.0.0 -> 8.1.0

--- pkgs/servers/atlassian/jira.nix
+++ pkgs/servers/atlassian/jira.nix
@@ -5,11 +5,11 @@
 
 stdenv.mkDerivation rec {
   name = "atlassian-jira-${version}";
-  version = "8.0.0";
+  version = "8.1.0";
 
   src = fetchurl {
     url = "https://product-downloads.atlassian.com/software/jira/downloads/atlassian-jira-software-${version}.tar.gz";
-    sha256 = "0ym2lz3215p6wxbw4nq87amiyjx30s150r973dr3jn1p4nvyh17i";
+    sha256 = "0mi1xknly44haf7gls3k212fx1dsl8k35rq82a1b3zj27kynwqr3";
   };
 
   buildPhase = ''
