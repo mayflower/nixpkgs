41f235b20a7501b51312bf435171583e3fdcb765
Robin Gloster <mail@glob.in>
2019-05-02 12:07:17 +0200

    fix cross eval but break libxmlPython

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11656,7 +11656,7 @@ in
 
   libxml2Python = pkgs.buildEnv { # slightly hacky
     name = "libxml2+py-${res.libxml2.version}";
-    paths = with libxml2; [ dev bin py ];
+    paths = with libxml2; [ dev bin /*py*/ ];
     inherit (libxml2) passthru;
     # the hook to find catalogs is hidden by buildEnv
     postBuild = ''
