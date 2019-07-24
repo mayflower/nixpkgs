5888ca3538d20a1e8530188af5af61f6395b864a
Robin Gloster <mail@glob.in>
2019-07-24 19:23:22 +0200

    gitlab: fix hook removal

--- nixos/modules/services/misc/gitlab.nix
+++ nixos/modules/services/misc/gitlab.nix
@@ -634,7 +634,7 @@ in {
         fi
 
         # We remove potentially broken links to old gitlab-shell versions
-        rm -f ${cfg.statePath}/repositories/**/*.git/hooks
+        rm -rf ${cfg.statePath}/repositories/**/*.git/hooks
 
         ${pkgs.sudo}/bin/sudo -u ${cfg.user} -H ${pkgs.git}/bin/git config --global core.autocrlf "input"
       '';
