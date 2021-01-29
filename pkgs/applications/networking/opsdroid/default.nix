f8b8be954f15fe8a8e398eb9024254bdab099af3
Robin Gloster <mail@glob.in>
2021-01-29 14:06:36 -0600

    opsdroid: 0.17.1 -> 0.20.1

--- pkgs/applications/networking/opsdroid/default.nix
+++ pkgs/applications/networking/opsdroid/default.nix
@@ -2,13 +2,13 @@
 
 pythonPackages.buildPythonPackage rec {
   pname = "opsdroid";
-  version = "0.17.1";
+  version = "0.20.1";
 
   src = fetchFromGitHub {
     owner = "opsdroid";
     repo = "opsdroid";
     rev = "v${version}";
-    sha256 = "15jlm74ir3qvshwkns856hhjhcp000zryh85bcj27dfnn6jn6snn";
+    sha256 = "14yjw6bjrxchzgm80947vl58p6wm6x6zfjmh9g9wi29g3vhvir89";
   };
 
   disabled = !pythonPackages.isPy3k;
@@ -17,41 +17,12 @@ pythonPackages.buildPythonPackage rec {
   doCheck = false;
 
   propagatedBuildInputs = with pythonPackages; [
-    Babel opsdroid_get_image_size slackclient webexteamssdk bleach
+    click Babel opsdroid_get_image_size slackclient webexteamssdk bleach
     parse emoji puremagic yamale nbformat websockets pycron nbconvert
-    aiohttp matrix-api-async aioredis aiosqlite arrow pyyaml motor
+    aiohttp matrix-api-async aioredis aiosqlite arrow pyyaml motor regex
     mattermostdriver setuptools voluptuous ibm-watson tailer multidict
+    watchgod get-video-properties appdirs bitstring
   ];
-
-  patches = [
-    # fixes shell connector, remove >0.17.1
-    (fetchpatch {
-      url = "https://github.com/opsdroid/opsdroid/commit/9c6eb3af14a2b5cd2ac875153d3707a490b48150.patch";
-      sha256 = "06gdlb6ysdg5w7m848k61bjqqnrrr3q82avkh9dv71nh4fqi6gv2";
-    })
-  ];
-
-  postPatch = ''
-    substituteInPlace requirements.txt \
-      --replace arrow==0.15.4 arrow>=0.15.4 \
-      --replace certifi==2019.11.28 certifi>=2019.11.28 \
-      --replace tailer==0.4.1 tailer>=0.4.1 \
-      --replace aioredis==1.3.1 aioredis>=1.3.1 \
-      --replace pyyaml==5.2 pyyaml>=5.2 \
-      --replace ibm-watson==4.0.1 ibm-watson>=4.0.1 \
-      --replace websockets==8.1 websockets>=8.1 \
-      --replace slackclient==2.5.0 slackclient>=2.5.0 \
-      --replace nbconvert==5.6.1 nbconvert>=5.6.1 \
-      --replace multidict==4.7.1 multidict>=4.7.1 \
-      --replace nbformat==4.4.0 nbformat>=4.4.0 \
-      --replace aiosqlite==0.10.0 aiosqlite>=0.10.0 \
-      ;
-    sed -i '/setuptools=/d' requirements.txt
-    substituteInPlace opsdroid/loader.py \
-      --replace \
-        'os.path.isfile(os.path.join(config["install_path"], "requirements.txt"))' \
-        "False"
-  '';
 
   passthru.python = pythonPackages.python;
 
