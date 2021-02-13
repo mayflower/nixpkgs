330d276f3bf034bd5c3f6d97dfdbccd1b959b4c1
Robbert Gurdeep Singh <git@beardhatcode.be>
2021-02-13 21:47:47 +0100

    matrix-appservice-slack: 1.4.0 -> 1.7.0
    (cherry picked from commit 88a38e21d623fe324d3e0f2a49610902db1abafa)

--- pkgs/servers/matrix-synapse/matrix-appservice-slack/generate-dependencies.sh
+++ pkgs/servers/matrix-synapse/matrix-appservice-slack/generate-dependencies.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-ROOT="$(realpath "$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")"/../../../..)"
+# Download package.json and package-lock.json from the v1.7.0 release
+curl https://raw.githubusercontent.com/matrix-org/matrix-appservice-slack/d589aa2e258213ec5ee61ab1d6205c4fb56d116d/package.json -o package.json
+curl https://raw.githubusercontent.com/matrix-org/matrix-appservice-slack/d589aa2e258213ec5ee61ab1d6205c4fb56d116d/package-lock.json -o package-lock.json
 
 $(nix-build $ROOT -A  nodePackages.node2nix --no-out-link)/bin/node2nix \
   --nodejs-12 \
