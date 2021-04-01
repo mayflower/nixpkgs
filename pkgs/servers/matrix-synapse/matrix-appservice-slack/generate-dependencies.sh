6a0ac6335f3e5059a2e0d9233b8525c712176b45
Robbert Gurdeep Singh <git@beardhatcode.be>
2021-04-01 20:45:56 +0200

    Use tag instead of sha to reference upstream pacakge.json
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>
    (cherry picked from commit 4c8b69bee32f13ec4a6f84e9337e89df9ca8034f)

--- pkgs/servers/matrix-synapse/matrix-appservice-slack/generate-dependencies.sh
+++ pkgs/servers/matrix-synapse/matrix-appservice-slack/generate-dependencies.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Download package.json and package-lock.json from the v1.7.0 release
-curl https://raw.githubusercontent.com/matrix-org/matrix-appservice-slack/d589aa2e258213ec5ee61ab1d6205c4fb56d116d/package.json -o package.json
-curl https://raw.githubusercontent.com/matrix-org/matrix-appservice-slack/d589aa2e258213ec5ee61ab1d6205c4fb56d116d/package-lock.json -o package-lock.json
+curl https://raw.githubusercontent.com/matrix-org/matrix-appservice-slack/1.7.0/package.json -o package.json
+curl https://raw.githubusercontent.com/matrix-org/matrix-appservice-slack/1.7.0/package-lock.json -o package-lock.json
 
 $(nix-build $ROOT -A  nodePackages.node2nix --no-out-link)/bin/node2nix \
   --nodejs-12 \
