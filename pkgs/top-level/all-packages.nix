51b1cbc0dd4d1b0a3f5dfc1cd53f9c28f2506fba
talyz <kim.lindberger@gmail.com>
2021-08-27 17:13:39 +0200

    gitlab-pages: Maintain together with the rest of GitLab + add to...
    ...update.py

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5403,6 +5403,8 @@ with pkgs;
     gitlabEnterprise = true;
   };
 
+  gitlab-pages = callPackage ../applications/version-management/gitlab/gitlab-pages { };
+
   gitlab-runner = callPackage ../development/tools/continuous-integration/gitlab-runner { };
 
   gitlab-shell = callPackage ../applications/version-management/gitlab/gitlab-shell { };
@@ -19822,8 +19824,6 @@ with pkgs;
   fusionInventory = callPackage ../servers/monitoring/fusion-inventory { };
 
   gatling = callPackage ../servers/http/gatling { };
-
-  gitlab-pages = callPackage ../servers/http/gitlab-pages { };
 
   glabels = callPackage ../applications/graphics/glabels { };
 
