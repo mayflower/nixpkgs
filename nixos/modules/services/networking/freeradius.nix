704a79e884673221ff04da1b4a92fc5e3f3199e5
Robin Gloster <mail@glob.in>
2021-12-16 18:57:38 +0100

    radius: fix evaluation by adding group

--- nixos/modules/services/networking/freeradius.nix
+++ nixos/modules/services/networking/freeradius.nix
@@ -74,7 +74,9 @@ in
         /*uid = config.ids.uids.radius;*/
         description = "Radius daemon user";
         isSystemUser = true;
+        group = "radius";
       };
+      groups.radius = {};
     };
 
     systemd.services.freeradius = freeradiusService cfg;
