065318ff07b023e0f5dddb10dd4dac5d31970d54
Robin Gloster <mail@glob.in>
2022-05-17 14:55:20 +0200

    lua5_2: add patch for CVE-2022-28805

--- pkgs/development/interpreters/lua-5/default.nix
+++ pkgs/development/interpreters/lua-5/default.nix
@@ -39,7 +39,9 @@ rec {
   lua5_2 = callPackage ./interpreter.nix {
     sourceVersion = { major = "5"; minor = "2"; patch = "4"; };
     hash = "0jwznq0l8qg9wh5grwg07b5cy3lzngvl5m2nl1ikp6vqssmf9qmr";
-    patches = lib.optional stdenv.isDarwin ./5.2.darwin.patch;
+    patches = [
+      ./lua-52-CVE-2022-28805.patch
+    ] ++ lib.optional stdenv.isDarwin ./5.2.darwin.patch;
   };
 
   lua5_2_compat = lua5_2.override({
