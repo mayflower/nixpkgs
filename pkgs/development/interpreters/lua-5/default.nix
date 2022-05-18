68bf65a9ca9a66bd23373441f90825c43adc01a9
Robin Gloster <mail@glob.in>
2022-05-18 14:16:05 +0200

    lua5_2: make patch path acceptable for vulnix

--- pkgs/development/interpreters/lua-5/default.nix
+++ pkgs/development/interpreters/lua-5/default.nix
@@ -43,7 +43,7 @@ rec {
     hash = "0jwznq0l8qg9wh5grwg07b5cy3lzngvl5m2nl1ikp6vqssmf9qmr";
     makeWrapper = makeBinaryWrapper;
     patches = [
-      ./CVE-2022-28805-lua52.patch
+      ./CVE-2022-28805.patch
     ] ++ lib.optional stdenv.isDarwin ./5.2.darwin.patch;
   };
 
