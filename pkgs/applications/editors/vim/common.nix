36b961af06b44b85175ee34f9c5db8bf5b6c7a5a
Nguyễn Gia Phong <mcsinyx@disroot.org>
2022-06-29 17:12:36 +0900

    vim: 8.2.5172 -> 9.0.0001
    (cherry picked from commit bb8d6d0bc6e636e1b5aff7c516455a2f976137e4)

--- pkgs/applications/editors/vim/common.nix
+++ pkgs/applications/editors/vim/common.nix
@@ -1,12 +1,12 @@
 { lib, fetchFromGitHub }:
 rec {
-  version = "8.2.5172";
+  version = "9.0.0001";
 
   src = fetchFromGitHub {
     owner = "vim";
     repo = "vim";
     rev = "v${version}";
-    sha256 = "sha256-ycp9K7IpXBFLE9DV9/iQ+N1H7EMD/tP/KGv2VOXoDvE=";
+    sha256 = "sha256-WnMm3q5Stn3s33rxQt76goURSa1Rq+jMVWYiS+uJTX0=";
   };
 
   enableParallelBuilding = true;
