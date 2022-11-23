a8db6795b31cdc0bdf5c4518107e5381a312e5fe
Robin Gloster <mail@glob.in>
2022-11-23 17:16:53 +0100

    gitlab: fix eval for non-pages case

--- nixos/modules/services/misc/gitlab.nix
+++ nixos/modules/services/misc/gitlab.nix
@@ -167,7 +167,7 @@ let
       };
       extra = {};
       uploads.storage_path = cfg.statePath;
-      pages = {
+      pages = lib.optionalAttrs cfg.pages.enable {
         enabled = cfg.pages.enable;
         port = 8090;
         host = cfg.pages.settings.pages-domain;
