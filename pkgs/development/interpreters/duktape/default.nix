c1f05fe02a224d062091518301307ddeb8a5cea1
Robin Gloster <mail@glob.in>
2023-08-04 19:16:14 +0200

    duktape: use env

--- pkgs/development/interpreters/duktape/default.nix
+++ pkgs/development/interpreters/duktape/default.nix
@@ -9,7 +9,7 @@ stdenv.mkDerivation rec {
   };
 
   # https://github.com/svaarala/duktape/issues/2464
-  LDFLAGS = [ "-lm" ];
+  env.LDFLAGS = "-lm";
 
   nativeBuildInputs = [ validatePkgConfig ];
 
