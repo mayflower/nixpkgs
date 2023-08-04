f99136b02470b62af958b0ba494e822e5306a45c
Robin Gloster <mail@glob.in>
2023-08-04 19:23:05 +0200

    python3.pkgs.python-dbusmock: use env

--- pkgs/development/python-modules/python-dbusmock/default.nix
+++ pkgs/development/python-modules/python-dbusmock/default.nix
@@ -29,7 +29,7 @@ in buildPythonPackage rec {
     hash = "sha256-AxRgoXPiFFFHQSj5/jU55hwWzHtutfjmD2IKGxYwd0A=";
   };
 
-  SETUPTOOLS_SCM_PRETEND_VERSION = version;
+  env.SETUPTOOLS_SCM_PRETEND_VERSION = version;
 
   nativeBuildInputs = [
     setuptools-scm
