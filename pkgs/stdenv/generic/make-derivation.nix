79191f0e3fe16bdb6e73a45455084502806289a6
Maximilian Bosch <maximilian@mbosch.me>
2023-09-09 19:05:13 +0200

    make-derivation: print out which drv has a wrongly typed attribute in `meta`

--- pkgs/stdenv/generic/make-derivation.nix
+++ pkgs/stdenv/generic/make-derivation.nix
@@ -505,7 +505,7 @@ else let
       "The ‘env’ attribute set cannot contain any attributes passed to derivation. The following attributes are overlapping: ${lib.concatStringsSep ", " overlappingNames}";
     lib.mapAttrs
       (n: v: assert lib.assertMsg (lib.isString v || lib.isPath v || lib.isBool v || lib.isInt v || lib.isDerivation v)
-        "The ‘env’ attribute set can only contain derivation, path, string, boolean or integer attributes. The ‘${n}’ attribute is of type ${builtins.typeOf v}."; v)
+        "${name}: The ‘env’ attribute set can only contain derivation, path, string, boolean or integer attributes. The ‘${n}’ attribute is of type ${builtins.typeOf v}."; v)
       env;
 
 in
