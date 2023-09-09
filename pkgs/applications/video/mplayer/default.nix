71189ca95b972f38973f688fdd7362e5acf4b9f3
Maximilian Bosch <maximilian@mbosch.me>
2023-09-09 16:15:15 +0200

    mplayer: use env

--- pkgs/applications/video/mplayer/default.nix
+++ pkgs/applications/video/mplayer/default.nix
@@ -176,7 +176,7 @@ stdenv.mkDerivation rec {
     echo CONFIG_MPEGAUDIODSP=yes >> config.mak
   '';
 
-  NIX_LDFLAGS = with lib; toString (
+  env.NIX_LDFLAGS = with lib; toString (
        optional  fontconfigSupport "-lfontconfig"
     ++ optional  fribidiSupport "-lfribidi"
     ++ optionals x11Support [ "-lX11" "-lXext" ]
