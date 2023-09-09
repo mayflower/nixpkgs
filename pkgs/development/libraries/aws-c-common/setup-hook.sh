f8f677962bf7485754128f06dec1c4585e6e2090
Robin Gloster <mail@glob.in>
2023-09-09 18:14:45 +0200

    aws-c-common: fix setup-hook with __structuredAttrs

--- pkgs/development/libraries/aws-c-common/setup-hook.sh
+++ pkgs/development/libraries/aws-c-common/setup-hook.sh
@@ -1,5 +1,9 @@
 addAwsCCommonModuleDir() {
-    cmakeFlags="-DCMAKE_MODULE_PATH=@out@/lib/cmake ${cmakeFlags:-}"
+    if [ -n "$__structuredAttrs" ]; then
+        cmakeFlags=("-DCMAKE_MODULE_PATH=@out@/lib/cmake" "${cmakeFlags[@]}")
+    else
+        cmakeFlags="-DCMAKE_MODULE_PATH=@out@/lib/cmake ${cmakeFlags:-}"
+    fi
 }
 
 postHooks+=(addAwsCCommonModuleDir)
