7e152b17b099fbf278a523afbebd0923a1c9133f
Maximilian Bosch <maximilian@mbosch.me>
2023-09-10 16:14:58 +0200

    nixos-rebuild: substituteAll -> runCommand
    Same reasoning for the change as with `pkgs.nixos-container`.

--- pkgs/os-specific/linux/nixos-rebuild/default.nix
+++ pkgs/os-specific/linux/nixos-rebuild/default.nix
@@ -1,4 +1,4 @@
-{ substituteAll
+{ runCommand
 , runtimeShell
 , coreutils
 , gnused
@@ -11,24 +11,18 @@
 let
   fallback = import ./../../../../nixos/modules/installer/tools/nix-fallback-paths.nix;
 in
-substituteAll {
-  name = "nixos-rebuild";
+runCommand "nixos-rebuild" {
   src = ./nixos-rebuild.sh;
-  dir = "bin";
-  isExecutable = true;
-  inherit runtimeShell nix;
-  nix_x86_64_linux = fallback.x86_64-linux;
-  nix_i686_linux = fallback.i686-linux;
-  nix_aarch64_linux = fallback.aarch64-linux;
-  path = lib.makeBinPath [ coreutils gnused gnugrep ];
   nativeBuildInputs = [
     installShellFiles
   ];
-  postInstall = ''
-    installManPage ${./nixos-rebuild.8}
-  '';
-
-  # run some a simple installer tests to make sure nixos-rebuild still works for them
+  env = {
+    inherit runtimeShell nix;
+    nix_x86_64_linux = fallback.x86_64-linux;
+    nix_i686_linux = fallback.i686-linux;
+    nix_aarch64_linux = fallback.aarch64-linux;
+    path = lib.makeBinPath [ coreutils gnused gnugrep ];
+  };
   passthru.tests = {
     simple-installer = nixosTests.installer.simple;
     specialisations = nixosTests.nixos-rebuild-specialisations;
@@ -42,3 +36,9 @@ substituteAll {
     mainProgram = "nixos-rebuild";
   };
 }
+''
+  mkdir -p $out/bin
+  substituteAll $src $out/bin/$name
+  chmod +x "$out"/bin/"$name"
+  installManPage ${./nixos-rebuild.8}
+''
