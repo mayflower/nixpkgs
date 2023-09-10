c7aa79f7f9636cdf8214e9d39946756fe1fc7fa5
WilliButz <willibutz@posteo.de>
2023-09-10 12:16:06 +0200

    llvm: fix lld with __structuredAttrs

--- pkgs/development/compilers/llvm/14/lld/default.nix
+++ pkgs/development/compilers/llvm/14/lld/default.nix
@@ -39,7 +39,7 @@ stdenv.mkDerivation rec {
   ];
 
   # Musl's default stack size is too small for lld to be able to link Firefox.
-  LDFLAGS = lib.optionalString stdenv.hostPlatform.isMusl "-Wl,-z,stack-size=2097152";
+  env.LDFLAGS = lib.optionalString stdenv.hostPlatform.isMusl "-Wl,-z,stack-size=2097152";
 
   outputs = [ "out" "lib" "dev" ];
 
