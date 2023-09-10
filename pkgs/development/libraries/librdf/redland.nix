c0db624fed29c803c32823b710c05519e3435ecd
WilliButz <willibutz@posteo.de>
2023-09-10 14:09:26 +0200

    redland: fix with __structuredAttrs

--- pkgs/development/libraries/librdf/redland.nix
+++ pkgs/development/libraries/librdf/redland.nix
@@ -35,7 +35,7 @@ stdenv.mkDerivation rec {
     ];
 
   # Fix broken DT_NEEDED in lib/redland/librdf_storage_sqlite.so.
-  NIX_CFLAGS_LINK = "-lraptor2";
+  env.NIX_CFLAGS_LINK = "-lraptor2";
 
   doCheck = false; # fails 1 out of 17 tests with a segmentation fault
 
