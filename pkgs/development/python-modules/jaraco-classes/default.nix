d91590f61d55a597e515bbf83d490c2ce4cc5a1f
Robin Gloster <mail@glob.in>
2023-09-12 00:12:13 +0200

    python.pkgs.jaraco-classes: fix with __structuredAttrs

--- pkgs/development/python-modules/jaraco-classes/default.nix
+++ pkgs/development/python-modules/jaraco-classes/default.nix
@@ -18,7 +18,7 @@ buildPythonPackage rec {
 
   pythonNamespaces = [ "jaraco" ];
 
-  SETUPTOOLS_SCM_PRETEND_VERSION = version;
+  env.SETUPTOOLS_SCM_PRETEND_VERSION = version;
 
   nativeBuildInputs = [ setuptools-scm ];
 
