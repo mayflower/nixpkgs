857cb73fb7984648459afdc889bce03370bf218b
Christian Albrecht <christian.albrecht@mayflower.de>
2023-10-31 10:07:28 +0100

    atlassian-confluence: 7.19.14 -> 7.19.16 (CVE-2023-22518)

--- pkgs/servers/atlassian/confluence.nix
+++ pkgs/servers/atlassian/confluence.nix
@@ -15,11 +15,11 @@ in
 optionalWarning (crowdProperties != null) "Using `crowdProperties` is deprecated!"
 (stdenvNoCC.mkDerivation rec {
   pname = "atlassian-confluence";
-  version = "7.19.14";
+  version = "7.19.16";
 
   src = fetchurl {
     url = "https://product-downloads.atlassian.com/software/confluence/downloads/${pname}-${version}.tar.gz";
-    sha256 = "sha256-Z4a4YZO9UnZSAZYB0FHRsX8QwX0ju3SeISsQquyA+w0=";
+    sha256 = "sha256-x8s6AAlJUXyKPpZJcT4WRfhfIp55vuVh9nUONduEj3A=";
   };
 
   buildPhase = ''
