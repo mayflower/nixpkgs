12b4ba6c6a01adaa5ede95f46acd2b43668e0489
Christian Albrecht <christian.albrecht@mayflower.de>
2023-10-31 10:07:28 +0100

    atlassian-confluence: 7.19.17
    atlassian-confluence: 7.19.14 -> 7.19.16 (CVE-2023-22518)
    
    (cherry picked from commit 857cb73fb7984648459afdc889bce03370bf218b)
    Signed-off-by: Christian Albrecht <christian.albrecht@mayflower.de>
    
    atlassian-confluence: 7.19.16 -> 7.19.17
    
    (cherry picked from commit 04c4ec43d1d28fa28616896b8afdd1cf89706b06)
    Signed-off-by: Christian Albrecht <christian.albrecht@mayflower.de>

--- pkgs/servers/atlassian/confluence.nix
+++ pkgs/servers/atlassian/confluence.nix
@@ -15,11 +15,11 @@ in
 optionalWarning (crowdProperties != null) "Using `crowdProperties` is deprecated!"
 (stdenvNoCC.mkDerivation rec {
   pname = "atlassian-confluence";
-  version = "7.19.14";
+  version = "7.19.17";
 
   src = fetchurl {
     url = "https://product-downloads.atlassian.com/software/confluence/downloads/${pname}-${version}.tar.gz";
-    sha256 = "sha256-Z4a4YZO9UnZSAZYB0FHRsX8QwX0ju3SeISsQquyA+w0=";
+    sha256 = "sha256-4URHuOCn6L/zuqRh/D8j9V6eeIgp9B0wqQ1ZbCfmYuA=";
   };
 
   buildPhase = ''
