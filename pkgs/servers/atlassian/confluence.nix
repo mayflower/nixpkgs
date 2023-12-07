04c4ec43d1d28fa28616896b8afdd1cf89706b06
Christian Albrecht <christian.albrecht@mayflower.de>
2023-12-07 10:19:03 +0100

    atlassian-confluence: 7.19.16 -> 7.19.17

--- pkgs/servers/atlassian/confluence.nix
+++ pkgs/servers/atlassian/confluence.nix
@@ -15,11 +15,11 @@ in
 optionalWarning (crowdProperties != null) "Using `crowdProperties` is deprecated!"
 (stdenvNoCC.mkDerivation rec {
   pname = "atlassian-confluence";
-  version = "7.19.16";
+  version = "7.19.17";
 
   src = fetchurl {
     url = "https://product-downloads.atlassian.com/software/confluence/downloads/${pname}-${version}.tar.gz";
-    sha256 = "sha256-x8s6AAlJUXyKPpZJcT4WRfhfIp55vuVh9nUONduEj3A=";
+    sha256 = "sha256-4URHuOCn6L/zuqRh/D8j9V6eeIgp9B0wqQ1ZbCfmYuA=";
   };
 
   buildPhase = ''
