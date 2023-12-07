4000e2c23bc583b6ee11589453ef1777d89008ed
Christian Albrecht <christian.albrecht@mayflower.de>
2023-12-07 10:25:23 +0100

    atlassian-jira: 9.11.1 -> 9.11.2

--- pkgs/servers/atlassian/jira.nix
+++ pkgs/servers/atlassian/jira.nix
@@ -8,11 +8,11 @@
 
 stdenv.mkDerivation rec {
   pname = "atlassian-jira";
-  version = "9.11.1";
+  version = "9.11.2";
 
   src = fetchurl {
     url = "https://product-downloads.atlassian.com/software/jira/downloads/atlassian-jira-software-${version}.tar.gz";
-    sha256 = "sha256-ertaJrURAMViQ5WVa8JgCu9vlTNwGVRiPTt7grIrgZQ=";
+    sha256 = "sha256-rzuy160loJuulPcai2vlFZsa0LCuxTe68IOeyuHBb5k=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
